108e6424590a37ce1db2ce977fe8a6ed085d32bf
Pim-fy <climent5373@naver.com>
2026-07-07 16:22:07 +0900

    프로젝트 생성

---
+++ nct-backend/src/main/java/nct/NctBackendApplication.java
@@ -0,0 +1,13 @@
+package nct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NctBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NctBackendApplication.class, args);
+	}
+
+}
---
+++ nct-backend/src/test/java/nct/NctBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NctBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
